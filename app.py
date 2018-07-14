2cdf1ac7d12a96cd7c8ca2a92ac3c88b2b224c21
rahulXs <rahulsharmaacn@gmail.com>
2018-07-14 20:12:30 +0530

    Update files

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ def index():
 
 @app.route('/success-table', methods=['POST'])
 def success_table():
-    return render_template()
+    if request.method == "POST":
+        file = request.files['file']
+
+        df = pandas.read_csv(file)
+        gc = Nominatim()
+        df["coordinates"] = df["Address"].apply(gc.geocode)
+        df['Latitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
+        df['Longitude'] = df['coordinates'].apply(lambda x: x.longitude if x != None else None)
+        df = df.drop("coordinates", 1)
+        df.to_csv("uploads/geocoded.csv", index=None)
+    return render_template("index.html", text=df.to_html(), btn='download.html')
 
 @app.route("/download-file/")
 def download():
